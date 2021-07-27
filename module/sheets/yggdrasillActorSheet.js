120141aece7637fef039c86658ea70fd9e9a3928
thibault <palportscar@hotmail.fr>
2021-07-27 21:56:33 +0200

    🚧 add filds to actor sheet

--- module/sheets/yggdrasillActorSheet.js
+++ module/sheets/yggdrasillActorSheet.js
@@ -1,5 +1,3 @@
-
-
 export default class YggdrasillActorSheet extends ActorSheet {
     get template(){
         console.log(`Yggdrasill | Récupération du fichier actor html ${this.actor.data.type}-sheet.html`);
@@ -9,9 +7,32 @@ export default class YggdrasillActorSheet extends ActorSheet {
 
     getData(){
         const data = super.getData();
-        data.systemData = data.data.data;
+        console.log(data);
+        this.setCalculatedData(data);
         console.log(data);
         return data;
     }
+
+    setCalculatedData = (data) => {
+        console.log("Yggdrasill | setCalculatedData");
+        data.data.data.ini.reaction = data.data.data.attributs.ame.instinct + data.data.data.attributs.esprit.perception + data.data.data.attributs.esprit.intelect;
+        console.log("Yggdrasill | reaction : "+data.data.data.ini.reaction);
+        data.data.data.dp.base = data.data.data.attributs.corps.agilité + data.data.data.attributs.corps.agilité + data.data.data.attributs.ame.instinct;
+        console.log("Yggdrasill | base defense physique : "+data.data.data.dp.base);
+        data.data.data.dm.base = data.data.data.attributs.esprit.tenacite + data.data.data.attributs.esprit.intelect + data.data.data.attributs.ame.instinct;
+        console.log("Yggdrasill | base defense mentale : "+data.data.data.dm.base);
+        
+        data.systemData = data.data.data;
+        return data;
+    }
+  /* -------------------------------------------- */
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if ( !this.isEditable ) return;
     
+  }
 }
