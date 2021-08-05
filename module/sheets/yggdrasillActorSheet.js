cadb72c4ba0775ec7ce87dcf8315eb4e32c48695
tperquis <tperquis@cesi.fr>
2021-08-05 15:21:09 +0200

    change to hbs extension file & add template to js

--- module/sheets/yggdrasillActorSheet.js
+++ module/sheets/yggdrasillActorSheet.js
@@ -1,45 +1,42 @@
 export default class YggdrasillActorSheet extends ActorSheet {
-    get template(){
-        console.log(`Yggdrasill | Récupération du fichier actor html ${this.actor.data.type}-sheet.html`);
+    static get defaultOptions(){
+        return mergeObject(super.defaultOptions, {
+            template: `systems/yggdrasill/templates/sheets/${this.actor.data.type}-sheet.hbs`,
+            classes: ["yggdrasill", "sheet", `${this.actor.data.type}`]
+        })
+    }
 
-        return `systems/yggdrasill/templates/sheets/${this.actor.data.type}-sheet.html`;
+    get template(){
+        console.log(`Yggdrasill | Récupération du fichier actor hbs ${this.actor.data.type}-sheet.hbs`);
+
+        return `systems/yggdrasill/templates/sheets/${this.actor.data.type}-sheet.hbs`;
     }
 
     getData(){
         const data = super.getData();
         console.log(data);
-        this.setCalculatedData(data);
+        // this.setCalculatedData(data);
         console.log(data);
         return data;
     }
 
-    setCalculatedData = (data) => {
-        console.log("Yggdrasill | setCalculatedData");
-        data.data.data.ini.reaction = data.data.data.attributs.ame.instinct + data.data.data.attributs.esprit.perception + data.data.data.attributs.esprit.intelect;
-        console.log("Yggdrasill | reaction : "+data.data.data.ini.reaction);
-        data.data.data.dp.base = data.data.data.attributs.corps.agilité + data.data.data.attributs.corps.agilité + data.data.data.attributs.ame.instinct;
-        console.log("Yggdrasill | base defense physique : "+data.data.data.dp.base);
-        data.data.data.dm.base = data.data.data.attributs.esprit.tenacite + data.data.data.attributs.esprit.intelect + data.data.data.attributs.ame.instinct;
-        console.log("Yggdrasill | base defense mentale : "+data.data.data.dm.base);
+    // setCalculatedData = (data) => {
+    //     console.log("Yggdrasill | setCalculatedData");
+    //     data.data.data.ini.reaction = data.data.data.attributs.ame.instinct + data.data.data.attributs.esprit.perception + data.data.data.attributs.esprit.intelect;
+    //     console.log("Yggdrasill | reaction : "+data.data.data.ini.reaction);
+    //     data.data.data.dp.base = data.data.data.attributs.corps.agilité + data.data.data.attributs.corps.agilité + data.data.data.attributs.ame.instinct;
+    //     console.log("Yggdrasill | base defense physique : "+data.data.data.dp.base);
+    //     data.data.data.dm.base = data.data.data.attributs.esprit.tenacite + data.data.data.attributs.esprit.intelect + data.data.data.attributs.ame.instinct;
+    //     console.log("Yggdrasill | base defense mentale : "+data.data.data.dm.base);
         
-        data.systemData = data.data.data;
-        return data;
-    }
+    //     data.systemData = data.data.data;
+    //     return data;
+    // }
   /* -------------------------------------------- */
 
   /** @override */
-  activateListeners(html) {
-    super.activateListeners(html);
+  activateListeners(hbs) {
+    super.activateListeners(hbs);
 
-    // Everything below here is only needed if the sheet is editable
-    // if ( !this.isEditable ) return;
-    html.find('.rollable').click(this._onRoll.bind(this));
-    
-  }
-  _onRoll(event){
-      console.log(event)
-      const dataSet = event.target.dataset["dice"];
-      console.log(dataSet)
-  }
 
 }
--- module/sheets/yggdrasillItemSheet.js
+++ module/sheets/yggdrasillItemSheet.js
@@ -8,9 +8,9 @@ export default class YggdrasillItemSheet extends ItemSheet {
     }
 
     get template(){
-        console.log(`Yggdrasill | Récupération du fichier html ${this.item.data.type}-sheet.html`);
+        console.log(`Yggdrasill | Récupération du fichier hbs ${this.item.data.type}-sheet.hbs`);
 
-        return `systems/yggdrasill/templates/sheets/${this.item.data.type}-sheet.html`;
+        return `systems/yggdrasill/templates/sheets/${this.item.data.type}-sheet.hbs`;
     }
 
     getData(){
